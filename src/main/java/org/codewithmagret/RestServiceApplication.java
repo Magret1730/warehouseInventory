3c7075b32b075db7306b0ba071b21f143b2fd279
Magret <belloabiodun17@gmail.com>
2026-04-03 10:49:10 -0230

    project setup

---
+++ src/main/java/org/codewithmagret/RestServiceApplication.java
@@ -0,0 +1,29 @@
+package org.codewithmagret;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main application class for the RESTful service,
+ * responsible for bootstrapping the Spring Boot application.
+ */
+@SpringBootApplication
+public class RestServiceApplication {
+
+    /**
+     * Default constructor for the RestServiceApplication class.
+     * This constructor is required by Spring Boot to create an instance of the application.
+     */
+    public RestServiceApplication() {
+    }
+
+    /**
+     * The entry point of the application, which starts the Spring Boot context.
+     *
+     * @param args command-line arguments (not used)
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(RestServiceApplication.class, args);
+    }
+}
+
